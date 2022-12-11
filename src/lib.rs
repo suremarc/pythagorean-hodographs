553b8746eb18c9eb109cc6811132a61bea8d98dc
suremarc <8771538+suremarc@users.noreply.github.com>
2022-12-10 18:01:53 -0600

    simplify api and fix docs

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ use glam::f32::*;
 
 use itertools::Itertools;
 
-// A twice-differentiable parametric curve in 3-space.
+/// A twice-differentiable parametric curve in 3-space.
 pub trait Curve {
     /// The value of this curve.
     fn p(&self, u: f32) -> Vec3;
@@ -23,13 +23,13 @@ pub trait Curve {
     }
 }
 
-// A parametric moving frame in 3-space.
+/// A parametric moving frame in 3-space.
 pub trait Frame {
     /// The value of moving frame in 3-space, containing both translation and rotation components.
     fn frame(&self, u: f32) -> Affine3A;
 }
 
-// A continuous curve built up from piecewise polynomials.
+/// A continuous curve built up from piecewise polynomials.
 pub struct Spline<T: Curve> {
     segments: Vec<T>,
 }
@@ -81,19 +81,101 @@ where
     }
 }
 
-#[derive(Copy, Clone, Debug)]
+#[derive(Clone, Debug)]
 pub struct QuinticPHCurve {
-    a0: Quat,
-    a1: Quat,
-    a2: Quat,
-    pi: Vec3,
+    data: QuinticPHData,
+    hermite: HermiteQuintic,
 }
 
 impl QuinticPHCurve {
     /// Construct a new quintic PH curve given C1 data.
     /// pi and pf are the initial and final positions, while
     /// di and df are the initial and final derivatives.
-    pub fn new(pi: Vec3, pf: Vec3, di: Vec3, df: Vec3) -> QuinticPHCurve {
+    pub fn new(pi: Vec3, pf: Vec3, di: Vec3, df: Vec3) -> Self {
+        let data = QuinticPHData::new(pi, pf, di, df);
+        let hermite = data.curve();
+        Self { data, hermite }
+    }
+}
+
+impl Curve for QuinticPHCurve {
+    #[inline]
+    fn p(&self, u: f32) -> Vec3 {
+        self.hermite.p(u)
+    }
+
+    #[inline]
+    fn dp(&self, u: f32) -> Vec3 {
+        self.hermite.dp(u)
+    }
+
+    #[inline]
+    fn d2p(&self, u: f32) -> Vec3 {
+        self.hermite.d2p(u)
+    }
+
+    #[inline]
+    fn speed(&self, u: f32) -> f32 {
+        self.hermite.speed(u)
+    }
+}
+
+impl Frame for QuinticPHCurve {
+    /// The Euler-Rodrigues frame for this PH curve.
+    /// Note that a spline consisting of Euler-Rodrigues frames is not necessarily continuous.
+    /// This is intended to be correct in future implementations, and R. Farouki provides
+    /// an algorithm to do so, but more research is needed.
+    fn frame(&self, u: f32) -> Affine3A {
+        let a = self.data.a0 * (1. - u).powi(2)
+            + self.data.a1 * u * (1. - u) * 2.
+            + self.data.a2 * u.powi(2);
+
+        let ai = (a * Quat::from_xyzw(1., 0., 0., 0.) * a.conjugate()).xyz() / a.length_squared();
+        let aj = (a * Quat::from_xyzw(0., 1., 0., 0.) * a.conjugate()).xyz() / a.length_squared();
+        let ak = (a * Quat::from_xyzw(0., 0., 1., 0.) * a.conjugate()).xyz() / a.length_squared();
+
+        Affine3A::from_mat3_translation(Mat3::from_cols(ai, aj, ak), self.hermite.p(u))
+    }
+}
+
+impl Spline<QuinticPHCurve> {
+    /// Construct a quintic PH spline from positional data, using the Catmull-Rom method
+    /// for the differential data.
+    pub fn catmull_rom(pts: Vec<Vec3>) -> Self {
+        let n = pts.len();
+        Self {
+            segments: pts
+                .into_iter()
+                .tuple_windows()
+                .map(|(p0, p1, p2)| (p1, 0.25 * ((n - 2) as f32) * (p2 - p0)))
+                .tuple_windows()
+                .map(|((p0, d0), (p1, d1))| QuinticPHCurve::new(p0, p1, d0, d1))
+                .collect(),
+        }
+    }
+
+    /// Construct a quintic PH spline given C1 interpolation data (position and derivative pairs).
+    pub fn hermite(data: Vec<[Vec3; 2]>) -> Self {
+        Self {
+            segments: data
+                .into_iter()
+                .tuple_windows()
+                .map(|([p0, d0], [p1, d1])| QuinticPHCurve::new(p0, p1, d0, d1))
+                .collect(),
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+struct QuinticPHData {
+    a0: Quat,
+    a1: Quat,
+    a2: Quat,
+    pi: Vec3,
+}
+
+impl QuinticPHData {
+    fn new(pi: Vec3, pf: Vec3, di: Vec3, df: Vec3) -> Self {
         let dp = pf - pi;
         let (q, _) = inverse_solve_quat(di + df);
         let q_normalized = q.normalize();
@@ -118,20 +200,12 @@ impl QuinticPHCurve {
                 (ta0, ta1, ta2)
             })
             .map(|(ta0, ta1, ta2)| (q_normalized * ta0, q_normalized * ta1, q_normalized * ta2))
-            .map(|(a0, a1, a2)| QuinticPHCurve { a0, a1, a2, pi })
+            .map(|(a0, a1, a2)| QuinticPHData { a0, a1, a2, pi })
             .min_by_key(|h| ordered_float::OrderedFloat(h.curve().elastic_bending_energy()))
             .unwrap()
     }
 
-    #[inline]
-    pub fn euler_rodrigues_frame(&self) -> EulerRodriguesFrame {
-        EulerRodriguesFrame {
-            data: *self,
-            curve: self.curve(),
-        }
-    }
-
-    pub fn curve(&self) -> HermiteQuintic {
+    fn curve(&self) -> HermiteQuintic {
         let a0len2 = self.a0.length_squared();
         let a1len2 = self.a1.length_squared();
         let a2len2 = self.a2.length_squared();
@@ -168,82 +242,9 @@ impl QuinticPHCurve {
     }
 }
 
-/// A special moving frame associated with pythagorean hodographs in 3-space,
-/// which is easy to compute.
-/// The Euler-Rodrigues frame of a spline is not necessarily continuous.
-pub struct EulerRodriguesFrame {
-    data: QuinticPHCurve,
-    curve: HermiteQuintic,
-}
-
-impl Curve for EulerRodriguesFrame {
-    #[inline]
-    fn p(&self, u: f32) -> Vec3 {
-        self.curve.p(u)
-    }
-
-    #[inline]
-    fn dp(&self, u: f32) -> Vec3 {
-        self.curve.dp(u)
-    }
-
-    #[inline]
-    fn d2p(&self, u: f32) -> Vec3 {
-        self.curve.d2p(u)
-    }
-
-    #[inline]
-    fn speed(&self, u: f32) -> f32 {
-        self.curve.speed(u)
-    }
-}
-
-impl Frame for EulerRodriguesFrame {
-    fn frame(&self, u: f32) -> Affine3A {
-        let a = self.data.a0 * (1. - u).powi(2)
-            + self.data.a1 * u * (1. - u) * 2.
-            + self.data.a2 * u.powi(2);
-
-        let ai = (a * Quat::from_xyzw(1., 0., 0., 0.) * a.conjugate()).xyz() / a.length_squared();
-        let aj = (a * Quat::from_xyzw(0., 1., 0., 0.) * a.conjugate()).xyz() / a.length_squared();
-        let ak = (a * Quat::from_xyzw(0., 0., 1., 0.) * a.conjugate()).xyz() / a.length_squared();
-
-        Affine3A::from_mat3_translation(Mat3::from_cols(ai, aj, ak), self.curve.p(u))
-    }
-}
-
-impl Spline<EulerRodriguesFrame> {
-    pub fn catmull_rom(pts: Vec<Vec3>) -> Self {
-        let n = pts.len();
-        Self {
-            segments: pts
-                .into_iter()
-                .tuple_windows()
-                .map(|(p0, p1, p2)| (p1, 0.125 * ((n - 2) as f32) * (p2 - p0)))
-                .tuple_windows()
-                .map(|((p0, d0), (p1, d1))| {
-                    QuinticPHCurve::new(p0, p1, d0, d1).euler_rodrigues_frame()
-                })
-                .collect(),
-        }
-    }
-
-    pub fn hermite(data: Vec<[Vec3; 2]>) -> Self {
-        Self {
-            segments: data
-                .into_iter()
-                .tuple_windows()
-                .map(|([p0, d0], [p1, d1])| {
-                    QuinticPHCurve::new(p0, p1, d0, d1).euler_rodrigues_frame()
-                })
-                .collect(),
-        }
-    }
-}
-
-// Hermite quintic polynomial obtained from the construction of a quintic PH spline.
+/// Hermite quintic polynomial obtained from the construction of a quintic PH spline.
 #[derive(Clone, Debug)]
-pub struct HermiteQuintic {
+struct HermiteQuintic {
     pi: Vec3,
     weights: [f32; 5],
     weighted_tangents: [Vec3; 5],
@@ -283,9 +284,9 @@ impl Curve for HermiteQuintic {
     }
 }
 
-// Generates a basis for the one-parameter family of solutions to AiA* = c.
-// This function returns the quaternions A1, A2 such that any solution
-// to AiA* = c can be generated by the solution A1 cos t + A2 sin t.
+/// Generates a basis for the one-parameter family of solutions to AiA* = c.
+/// This function returns the quaternions A1, A2 such that any solution
+/// to AiA* = c can be generated by the solution A1 cos t + A2 sin t.
 fn inverse_solve_quat(c: Vec3) -> (Quat, Quat) {
     let [lambda, mu, nu] = c.normalize().to_array();
     let r = (0.5 * (1. + lambda) * c.length()).sqrt();
